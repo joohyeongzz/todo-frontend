b70b55c591536e0127db50413e2c71de0441edeb
Kang, Jooheong <joohyeongzz@naver.com>
2024-08-06 01:21:39 +0000

    asd

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === "localhost") {
   backendHost = "http://localhost:8080";
 } else {
-  backendHost = "http://www.awskjh.p-e.kr";
+  backendHost = "https://www.awskjh.p-e.kr";
 }
 
 export const API_BASE_URL = `${backendHost}`;
